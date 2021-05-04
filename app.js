0b9ee13ea580ae1b25ec6a5812bc647c57e96ba4
cris-mc <martinezcalderon.ci@gmail.com>
2021-05-04 15:25:18 -0300

    corrgiendo listen

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require('path');
 
 
-app.listen(process.env.PORT || 3000, ()=> console.log('Servidor corriendo OK'));
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Servidor corriendo OK');
+});
 
 const publicPath = path.resolve(__dirname, './public');
 
